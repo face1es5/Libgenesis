4f8710674c871e09bf6de2de08f8f2425407701f
paramecium <f4ce1ess@outlook.com>
2024-08-27 10:00:01 +0800

    Initial Commit

---
+++ Libgenesis/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Libgenesis
+//
+//  Created by Fish on 27/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Libgenesis/LibgenesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibgenesisApp.swift
+//  Libgenesis
+//
+//  Created by Fish on 27/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct LibgenesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
